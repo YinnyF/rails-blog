19864111647185d9e33d9e565d67a88f7e042981
cynthia <yinnycfu@gmail.com>
2021-09-11 22:36:31 +0100

    feat: add new and create actions to ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # this instantiates a new article but does not save it.
+  # will be used in the view when building the form
+  def new
+    @article = Article.new
+  end
+
+  # instantiates a new article with values and then attempts to save it.
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      # redirects the browser to the article's page at "http://localhost:3000/articles/#{@article.id}".
+      redirect_to @article
+    else
+      # redirects to the form again
+      render :new
+    end
+  end
+
 end
