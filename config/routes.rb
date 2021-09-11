7b665647f18e9e7128c92db1a4cb380a791d2b3d
cynthia <yinnycfu@gmail.com>
2021-09-11 19:48:20 +0100

    feat: set the app homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Set the app homepage to /articles
+  root "articles#index"
+  
   # Create route to GET /articles which is mapped to the `index` action of `ArticlesController`
   get "/articles", to: "articles#index"
 end
