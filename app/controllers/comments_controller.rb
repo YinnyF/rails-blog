b776b2176172a8451a90ba68cd74c607dc5db741
cynthia <yinnycfu@gmail.com>
2021-09-11 23:39:02 +0100

    feat: create action on CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    # the complexity is a side-effect of the nesting that you've set up
+    # Each request for a comment has to keep track of the article to which the comment is attached
+    @article = Article.find(params[:article_id])
+    # .create: This will automatically link the comment so that it belongs to that particular article.
+    @comment = @article.comments.create(comment_params)
+    # essentially refreshes the page
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
