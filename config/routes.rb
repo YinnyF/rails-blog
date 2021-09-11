db9325139817410d7ab432f68120f64401feb265
cynthia <yinnycfu@gmail.com>
2021-09-11 23:27:14 +0100

    feat: add comments as a nested resource in articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root "articles#index"
   
   # resources maps all of the conventional routes for a collection of resources
-  resources :articles
+  resources :articles do
+    # This creates comments as a nested resource within articles. This is another part of capturing the hierarchical relationship that exists between articles and comments.
+    resources :comments
+  end
 end
