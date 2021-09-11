4972a843904ed0f0d5fc6c3f08b11a1353e825b5
cynthia <yinnycfu@gmail.com>
2021-09-11 19:39:05 +0100

    feat: articles route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create route to GET /articles which is mapped to the `index` action of `ArticlesController`
+  get "/articles", to: "articles#index"
 end
